d90207b3abc9f1226270016debdba8e5a4b2474e
Kubister11 <mnowslukaszewicz@gmail.com>
2026-05-28 03:13:49 +0200

    feat: added mason with clang

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,61 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    {
+        "mason-org/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "clangd" },
+            })
+        end,
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            require("lspconfig").clangd.setup({
+                capabilities = capabilities,
+            })
+        end,
+    },
+
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+
+        config = function()
+            local cmp = require("cmp")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                }),
+
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }),
+            })
+        end,
+    },
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,4 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
